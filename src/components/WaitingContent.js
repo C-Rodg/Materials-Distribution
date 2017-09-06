240f3b58b7832ab72ff3698ce1d78adc0a7bf49f
C-Rodg <rodgersbc@gmail.com>
2017-09-06 16:44:57 -0700

    Started loader animations

--- src/components/WaitingContent.js
+++ src/components/WaitingContent.js
@@ -5,11 +5,22 @@ const WaitingContent = ({ onStartScan, onStopScan }) => {
 	return (
 		<div className="waiting-content">
 			<div className="waiting-info">
-				<div className="waiting-text-container">
+				<div className="waiting-loading-container">
 					<div className="waiting-icon">
 						<i className="material-icons">hourglass_empty</i>
 					</div>
 					<div className="waiting-text">Waiting for Scan...</div>
+					<svg className="circular-loader" viewBox="25 25 50 50">
+						<circle
+							className="loader-path"
+							cx="50"
+							cy="50"
+							r="24"
+							fill="none"
+							stroke="#FFFFFF"
+							strokeWidth="1"
+						/>
+					</svg>
 				</div>
 			</div>
 			<div className="start-scan">
