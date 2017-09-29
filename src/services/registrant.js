dd988a9ce908a85d459fc470ed364d3a8f743b89
C-Rodg <rodgersbc@gmail.com>
2017-09-28 17:20:24 -0700

    Started working on translation convertion function

--- src/services/registrant.js
+++ src/services/registrant.js
@@ -15,15 +15,69 @@ function Registrant() {
 	this.qrLastName = "";
 	this.qrRegId = "";
 	this.qrHasAttended = false;
+	this.items = [];
+}
+const baseFields = ["qrFirstName", "qrLastName", "qrRegId", "qrHasAttended"];
+const allowedGiftTrueFalseFields = ["qrGiftAmexWed", "qrGiftAmexThurs"];
+const allowedGiftValueFields = ["qrGiftShirt"];
+const alreadyGotFields = [
+	"qrPickedUpAmexWed",
+	"qrPickedUpAmexThurs",
+	"qrPickedUpShirt",
+	"qrPickedUpGiftB"
+];
+const configItems = [
+	{
+		type: "TF",
+		name: "American Express - Wednesday",
+		lcTag: "lcPickedUpAmexWed",
+		pwsTag: "qrPickedUpAmexWed",
+		allowTag: "qrGiftAmexWed"
+	},
+	{
+		type: "TF",
+		name: "American Express - Thursday",
+		lcTag: "lcPickedUpAmexThurs",
+		pwsTag: "qrPickedUpAmexThurs",
+		allowTag: "qrGiftAmexThurs"
+	},
+	{
+		type: "TF",
+		name: "T-Shirt ",
+		lcTag: "lcPickedUpShirt",
+		pwsTag: "qrPickedUpShirt",
+		allowTag: "qrGiftShirt"
+	},
+	{
+		type: "SWITCH",
+		lcTag: "lcPickedUpGiftB",
+		pwsTag: "qrPickedUpGiftB",
+		allowTag: "qrPickedUpGiftB",
+		valOne: "Oversized Truck",
+		valTwo: "Hat"
+	}
+];
 
-	this.qrGiftAmexWed = "";
-	this.qrGiftAmexThurs = "";
-	this.qrGiftShirt = "";
+function TrueFalseItem(obj) {
+	this.type = "TF";
+	this.name = obj.name;
+	this.lcTag = obj.lcTag;
+	this.pwsTag = obj.pwsTag;
+	this.allowTag = obj.allowTag;
+	this.hasPickedUp = false;
+	this.disabled = true;
+}
 
-	this.qrPickedUpAmexWed = "";
-	this.qrPickedUpAmexThurs = "";
-	this.qrPickedUpShirt = "";
-	this.qrPickedUpGiftB = "";
+function SwitchItem(obj) {
+	this.type = "SWITCH";
+	this.lcTag = obj.lcTag;
+	this.pwsTag = obj.pwsTag;
+	this.allowTag = obj.allowTag;
+	this.valOne = obj.valOne;
+	this.valTwo = obj.valTwo;
+	this.selected = "";
+	this.disabled = false;
+	this.hasPickedUp = false;
 }
 
 // Call external service to get translation
@@ -90,11 +144,63 @@ export const convertTranslationToRegistrant = translation => {
 	return new Promise((resolve, reject) => {
 		if (translation.DataItems && translation.DataItems.length > 0) {
 			const registrant = new Registrant();
-			translation.DataItems.forEach(item => {
-				if (registrant.hasOwnProperty(item.Id)) {
-					registrant[item.Id] = item.Value;
+
+			// Create base items
+			registrant.items = configItems.map(configItem => {
+				if (configItem.type === "TF") {
+					return new TrueFalseItem(configItem);
+				} else if (configItem.type === "SWITCH") {
+					return new SwitchItem(configItem);
 				}
 			});
+
+			// SWITCH TO LOOPING THROUGH DECLARATIONS...
+			// GETTING VALUE FROM DATAITEM...
+			// SETTING REGISTRANT VALUE...
+			// START HERE
+			translation.Declarations.forEach(decItem => {
+				if (baseFields.indexOf(item.Id) > -1) {
+				}
+			});
+
+			translation.DataItems.forEach(item => {
+				if (baseFields.indexOf(item.Id) > -1) {
+					// Mark Base Registrant props
+					registrant[item.Id] = item.Value;
+				} else if (allowedGiftTrueFalseFields.indexOf(item.Id) > -1) {
+					// Mark not 'disabled' prop for yes/no items
+					if (item.Value.toUpperCase() === "YES") {
+						const idx = registrant.items.findIndex(
+							regItem => regItem.allowTag === item.Id
+						);
+						registrant.items[idx].disabled = false;
+					}
+				} else if (allowedGiftValueFields.indexOf(item.Id) > -1) {
+					// Mark not 'disabled' and change name for value items
+					if (item.Value) {
+						const idx = registrant.items.findIndex(
+							regItem => regItem.allowTag === item.Id
+						);
+						registrant.items[idx].disabled = false;
+						registrant.items[idx].name += `${item.Value}`;
+					}
+				} else if (alreadyGotFields.indexOf(item.Id) > -1) {
+					// Mark 'hasPickedUp'
+					if (item.Value) {
+						const idx = registrant.items.findIndex(
+							regItem => regItem.pwsTag === item.Id
+						);
+						if (registrant.items[idx].type === "TF") {
+							registrant.items[idx].hasPickedUp = true;
+						} else if (registrant.items[idx].type === "SWITCH") {
+							registrant.items[idx].selected = item.Value;
+							registrant.items[idx].disabled = true;
+						}
+					}
+				}
+			});
+			// TESTING - resolve for now
+			resolve(registrant);
 			if (!registrant["qrRegId"]) {
 				reject({ message: "No registrant ID associated with this record..." });
 			}
